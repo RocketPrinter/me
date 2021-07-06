910dda0faeb33d47f65d31d7cbe7c14b921fa843
RocketPrinter <calinioncioaia@gmail.com>
2021-07-06 17:05:39 +0300

    cleanup

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using BlazorStrap;
+using BlazorStyled;
 
 namespace PersonalSite
 {
@@ -20,6 +21,7 @@ namespace PersonalSite
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBootstrapCss();
+            builder.Services.AddBlazorStyled();
 
             await builder.Build().RunAsync();
         }
